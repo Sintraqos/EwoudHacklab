34935e487badddef5fb611f642518e231eec1f0d
Sintraqos <sintraqos@gmail.com>
2024-04-08 23:08:02 +0200

    Update Homework Lesson 3
    Added lesson 3

--- Homework/src/Main.java
+++ Homework/src/Main.java
@@ -1,6 +1,7 @@
 import java.util.HashMap;
 import java.text.MessageFormat;
 
+
 public class Main
 {
     public void main(String[] args)
@@ -12,7 +13,10 @@ public class Main
 
     void ChooseLesson()
     {
-        println("Enter lesson you want to open:\nIntro Lesson 1\nIntro Lesson 2\nIntro Lesson 3\n1\n2");
+        System.out.println("Enter lesson you want to open:");
+        System.out.println("Lesson 01 - How Far Away Is The Lightning");
+        System.out.println("Lesson 02 - Add / Remove Students");
+
         switch(System.console().readLine().toLowerCase())
         {
             case "intro lesson 1":
@@ -47,26 +51,26 @@ public class Main
 
     void IntroLesson01() {
         // Create number list and check if the values are even or odd
-        int[] _numberList = new int[]{42, 67, 35, 89, 24, 76, 58, 93, 7, 30, 83, 46, 13, 25, 98, 53, 17, 79, 57, 8};
+        int[] numberList = new int[]{42, 67, 35, 89, 24, 76, 58, 93, 7, 30, 83, 46, 13, 25, 98, 53, 17, 79, 57, 8};
         // HasMap Settings
-        String _evenKey = "Even";
-        String _oddKey = "Odd";
-        HashMap<String, Integer> _numbers = new HashMap<String, Integer>() {{
-            put(_evenKey, 0);
-            put(_oddKey, 0);
+        String evenKey = "Even";
+        String oddKey = "Odd";
+        HashMap<String, Integer> numbers = new HashMap<String, Integer>() {{
+            put(evenKey, 0);
+            put(oddKey, 0);
         }};
 
         // Loop through the numberList
-        for (int _number : _numberList) {
+        for (int number : numberList) {
             // If _number is even
-            if (_number % 2 == 0)
-                _numbers.put(_evenKey, _numbers.get(_evenKey) + 1);
+            if (number % 2 == 0)
+                numbers.put(evenKey, numbers.get(evenKey) + 1);
             else
-                _numbers.put(_oddKey, _numbers.get(_oddKey) + 1);
+                numbers.put(oddKey, numbers.get(oddKey) + 1);
         }
 
         // Print the hashmap
-        System.out.println(_numbers);
+        System.out.println(numbers);
 
         // Await input, then return to ChooseLesson()
         System.console().readLine();
@@ -80,13 +84,13 @@ public class Main
 
     void IntroLesson02()
     {
-        float _gallons;
-        float _liters;
+        float gallons;
+        float liters;
 
-        _gallons = 12;
-        _liters = _gallons * 3.7854f;
+        gallons = 12;
+        liters = gallons * 3.7854f;
 
-        println(MessageFormat.format("{0} gallons is equal to: {1} liters", _gallons, _liters));
+        System.out.println(MessageFormat.format("{0} gallons is equal to: {1} liters", gallons, liters));
 
         // Await input, then return to ChooseLesson()
         System.console().readLine();
@@ -99,19 +103,19 @@ public class Main
     // There are 3.7854 liters in a gallon
 
     void IntroLesson03() {
-        float _gallons = 100;
-        float _liters = 0;
-        int _counter = 0;
+        float gallons = 100;
+        float liters = 0;
+        int counter = 0;
 
-        for (float _currentGallon = 0; _currentGallon <= _gallons; _currentGallon++) {
-            _liters = _currentGallon * 3.7854f;
-            println(MessageFormat.format("{0} gallons is equal to: {1} liters", _currentGallon, _liters));
+        for (float currentGallon = 0; currentGallon <= gallons; currentGallon++) {
+            liters = currentGallon * 3.7854f;
+            System.out.println(MessageFormat.format("{0} gallons is equal to: {1} liters", currentGallon, liters));
 
             // Add to the counter, reset if it reaches 10
-            _counter++;
-            if (_counter >= 10) {
+            counter++;
+            if (counter >= 10) {
                 System.out.println();
-                _counter = 0;
+                counter = 0;
             }
         }
 
@@ -126,19 +130,18 @@ public class Main
 
     void Lesson01()
     {
-        println("#####################");
-        println("Lesson 01 - How Far Away Is The Lightning");
+        System.out.println("Lesson 01 - How Far Away Is The Lightning");
 
         // Speed of sound in M/s 343
         // Speed of sound in Ft/s 1100
-        float _distance;
-        float _time;
+        float distance;
+        float time;
 
-        println("Enter The Time:");
+        System.out.println("Enter The Time:");
 
         try
         {
-            _time = Float.parseFloat(System.console().readLine());
+            time = Float.parseFloat(System.console().readLine());
         }
         catch (NumberFormatException _exception)
         {
@@ -149,29 +152,19 @@ public class Main
             return;
         }
 
-        println("Enter The Distance Type You Want:\nft / feet\nm / meter");
-        float _distanceMultiplier = 0;
-        String _distanceType = "";
+        System.out.println("Enter The Distance Type You Want:\nft / feet\nm / meter");
+        float distanceMultiplier = 0;
+        String distanceType = "";
         // Switch over the user input
         switch (System.console().readLine().toLowerCase()) {
-            case "m":
-                _distanceMultiplier = 343;
-                _distanceType = "meters";
+            case "m", "meter":
+                distanceMultiplier = 343;
+                distanceType = "meters";
                 break;
 
-            case "meter":
-                _distanceMultiplier = 343;
-                _distanceType = "meters";
-                break;
-
-            case "feet":
-                _distanceMultiplier = 1100;
-                _distanceType = "feet";
-                break;
-
-            case "ft":
-                _distanceMultiplier = 1100;
-                _distanceType = "feet";
+            case "feet", "ft":
+                distanceMultiplier = 1100;
+                distanceType = "feet";
                 break;
 
             default:
@@ -179,9 +172,9 @@ public class Main
                 break;
         }
 
-        _distance = _time * _distanceMultiplier;
+        distance = time * distanceMultiplier;
 
-        println(MessageFormat.format("The lightning is: {0} {1} away", _distance, _distanceType));
+        System.out.println(MessageFormat.format("The lightning is: {0} {1} away", distance, distanceType));
 
         // Await input, then return to ChooseLesson()
         System.console().readLine();
@@ -194,20 +187,19 @@ public class Main
 
     void Lesson02()
     {
-        println("#####################");
-        println("Lesson 02 - Add / Remove Students");
+        System.out.println("Lesson 02 - Add / Remove Students");
 
         Lesson02_StudentFunctions(new HashMap<String, Integer>());
     }
 
     void Lesson02_StudentFunctions(HashMap<String, Integer> _students)
     {
-        println("Enter the number of what you want to do:\nAdd Student: 1\nRemove Student: 2\nShow Students: 3");
+        System.out.println("Enter the number of what you want to do:\nAdd Student: 1\nRemove Student: 2\nShow Students: 3");
 
         switch (System.console().readLine().toLowerCase())
         {
             case "1":   // Add Student
-                println("Write out the student name and age, IE: John Smith 99");
+                System.out.println("Write out the student name and age, IE: John Smith 99");
                 // Get the userinput
                 String _addStudentUserInput = System.console().readLine();
                 // Split the user input by spaces
@@ -246,7 +238,7 @@ public class Main
                 break;
 
             case "2":   // Remove Student
-                println("Write out the student name, IE: John Smith");
+                System.out.println("Write out the student name, IE: John Smith");
                 // Get userinput for name of the student
                 String _removeStudentUserInput = System.console().readLine();
 
@@ -254,7 +246,7 @@ public class Main
                 if(!_students.containsKey(_removeStudentUserInput))
                 {
                     // if it doesn't exist in the hashmap wait for userinput and return to the start of this lesson
-                    println("Invalid Student Name: " + _removeStudentUserInput);
+                    System.out.println("Invalid Student Name: " + _removeStudentUserInput);
                     System.console().readLine();
                     Lesson02_StudentFunctions(_students);
                 }
@@ -278,15 +270,6 @@ public class Main
         }
 
         Lesson02_StudentFunctions(_students);
-    }
-
-    //endregion
-
-    //region Shared Functions
-
-    void println(String _string)
-    {
-        System.out.println(_string);
     }
 
     //endregion
